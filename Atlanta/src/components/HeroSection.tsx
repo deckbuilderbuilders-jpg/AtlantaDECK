a70fa35a16c78122594e54b5c122480c08c3cec4
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-07 13:17:21 -0400

    Update HeroSection.tsx
    added button "scroll to process" for 2nd CTA

--- Atlanta/src/components/HeroSection.tsx
+++ Atlanta/src/components/HeroSection.tsx
@@ -24,7 +24,12 @@ const HeroSection = () => {
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
           <QuoteModal />  {/* ← CHANGED FROM BUTTON */}
-          <Button variant="secondary" size="lg" className="px-8 py-6 text-lg">
+          <Button 
+            variant="secondary" 
+            size="lg" 
+            className="px-8 py-6 text-lg"
+            onClick={scrollToProcess}
+          >
             How We're Different
           </Button>
         </div>
