03a10e2b8af145848c9fcee0e48272330340065a
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-08 15:54:25 -0400

    Update Index.tsx

--- Atlanta/src/pages/Index.tsx
+++ Atlanta/src/pages/Index.tsx
@@ -12,8 +12,8 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Header />
       <HeroSection />
-      <AboutSection />
       <ServicesSection />
+      <AboutSection />
       <ProcessSection />
       <FAQSection />
       <CTASection />
