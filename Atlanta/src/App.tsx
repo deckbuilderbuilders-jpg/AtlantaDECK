e0cc59ae9d29fec8c7e8ba5080c4165ddd63450a
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-20 10:46:11 -0400

    Update App.tsx

--- Atlanta/src/App.tsx
+++ Atlanta/src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Locations from "./pages/Locations";
+import DeckRepairs from "./pages/DeckRepairs"; // Add this import
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/locations" element={<Locations />} />
-          <Route path="/deck-repairs" element={<DeckRepairs />} /> {/* Add this line */}
+          <Route path="/deck-repairs" element={<DeckRepairs />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
