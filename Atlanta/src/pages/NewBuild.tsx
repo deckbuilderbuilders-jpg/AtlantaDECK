163a221ff14d0ac8fab208744438fc7bee63ad71
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-21 08:44:13 -0400

    Update NewBuild.tsx

--- Atlanta/src/pages/NewBuild.tsx
+++ Atlanta/src/pages/NewBuild.tsx
@@ -1,9 +1,12 @@
+import Header from "@/components/Header"
 import { DeckHero } from "@/components/deck-hero"
 import { DeckBuildProcess } from "@/components/deck-build-process"
 import { DeckDesignOptions } from "@/components/deck-design-options"
 import { ServicePromise } from "@/components/service-promise"
 import { LocationsSection } from "@/components/locations-section"
 import FAQSection from "@/components/FAQSection"
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
