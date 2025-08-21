cd0903f1fd0683a5938c8b2f5e3a03a22e4deff2
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-21 08:37:01 -0400

    Update App.tsx
    working?

--- Atlanta/src/App.tsx
+++ Atlanta/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Locations from "./pages/Locations";
 import DeckRepairs from "./pages/DeckRepairs"; // Add this import
+import NewBuild from "./pages/NewBuild";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/locations" element={<Locations />} />
           <Route path="/deck-repairs" element={<DeckRepairs />} />
+          <Route path="/new-decks" element={<NewBuild />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
