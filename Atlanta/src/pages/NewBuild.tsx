a7fb99889008a905fbf2f716043c70a2866823fd
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-21 10:44:58 -0400

    Update NewBuild.tsx

--- Atlanta/src/pages/NewBuild.tsx
+++ Atlanta/src/pages/NewBuild.tsx
@@ -12,7 +12,6 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
-      <DeckHero />
       <DeckDesignOptions />
       <DeckBuildProcess />
       <ServicePromise />
