6f8406acddc58e4a539bcde8a26a6b71311ba4a9
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-21 10:44:46 -0400

    Update DeckRepairs.tsx

--- Atlanta/src/pages/DeckRepairs.tsx
+++ Atlanta/src/pages/DeckRepairs.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
-      <DeckHero />
       <CommonRepairs />
       <DeckProcess />
       <ServicePromise />
