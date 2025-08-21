39e7aea1d9ac50c4f7baa852efbc001289d5a167
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-21 08:43:23 -0400

    Update DeckRepairs.tsx

--- Atlanta/src/pages/DeckRepairs.tsx
+++ Atlanta/src/pages/DeckRepairs.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header";
-import { DeckHero } from "@/components/deck-hero"
-import { DeckProcess } from "@/components/deck-process"
-import { CommonRepairs } from "@/components/common-repairs"
-import { ServicePromise } from "@/components/service-promise"
-import { LocationsSection } from "@/components/locations-section"
-import FAQSection from "@/components/FAQSection"
+import { DeckHero } from "@/components/deck-hero";
+import { DeckProcess } from "@/components/deck-process";
+import { CommonRepairs } from "@/components/common-repairs";
+import { ServicePromise } from "@/components/service-promise";
+import { LocationsSection } from "@/components/locations-section";
+import FAQSection from "@/components/FAQSection";
 
 export default function Home() {
   return (
