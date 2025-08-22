ec92afe7e81ae62d2daaaccf7ac26d25ca882f01
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 07:48:30 -0400

    Update DeckUpgrades.tsx

--- Atlanta/src/components/DeckUpgrades.tsx
+++ Atlanta/src/components/DeckUpgrades.tsx
@@ -45,7 +45,7 @@ const upgradeServices = [
   },
 ]
 
-export defualt function DeckUpgrades() {
+export default function DeckUpgrades() {
   return (
     <section className="py-16 px-4 bg-white">
       <div className="max-w-7xl mx-auto">
