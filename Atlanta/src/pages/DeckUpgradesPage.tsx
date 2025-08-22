14c1cb22961384375cd669f8bcdbe71fc7902ff8
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 07:36:39 -0400

    Update DeckUpgradesPage.tsx

--- Atlanta/src/pages/DeckUpgradesPage.tsx
+++ Atlanta/src/pages/DeckUpgradesPage.tsx
@@ -1,1 +1,24 @@
+import Header from "@/components/Header"
+import { DeckRepairProcess } from "@/components/deck-repair-process"
+import { DeckUpgrades} from "@/components/deck-upgrades"
+import { DeckProcess } from "@/components/deck-process"
+import { ServicePromise } from "@/components/service-promise"
+import { LocationsSection } from "@/components/locations-section"
+import FAQSection from "@/components/FAQSection"
+import Footer from "@/components/Footer"
 
+const CommonRepairs = () => {
+   return (
+    <main className="min-h-screen">
+      <Header />
+      <DeckRepairs />
+      <DeckProcess />
+      <ServicePromise />
+      <LocationsSection />
+      <FAQSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default CommonRepairs;
