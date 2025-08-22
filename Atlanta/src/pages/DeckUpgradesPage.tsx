06dd7b007c86dd0cc66f0978c6635657330e3a1b
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 08:00:43 -0400

    Update DeckUpgradesPage.tsx

--- Atlanta/src/pages/DeckUpgradesPage.tsx
+++ Atlanta/src/pages/DeckUpgradesPage.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header"
 import DeckUpgrades from "@/components/DeckUpgrades"
 import { DeckRepairProcess } from "@/components/deck-repair-process"
+import { DeckRepairs } from "@/components/deck-repairs"
 import { DeckProcess } from "@/components/deck-process"
 import { ServicePromise } from "@/components/service-promise"
 import { LocationsSection } from "@/components/locations-section"
