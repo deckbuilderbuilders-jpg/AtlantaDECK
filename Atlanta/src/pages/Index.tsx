8a9050229363aa04e119441482b8a9f36d69fb9f
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 07:15:18 -0400

    Update Index.tsx

--- Atlanta/src/pages/Index.tsx
+++ Atlanta/src/pages/Index.tsx
@@ -6,7 +6,7 @@ import FAQSection from "@/components/FAQSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
 import DeckSolutionsSection from "@/components/deck-solutions-section";
-import AboutSection from "@/components/about-section";
+import AboutSection from "@/components/AboutSection";
 
 
 const Index = () => {
