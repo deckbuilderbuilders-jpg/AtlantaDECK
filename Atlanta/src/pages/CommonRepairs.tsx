c46e40ec6e06a242dbdbdcab25c9505fc483bd0f
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 07:09:24 -0400

    Update CommonRepairs.tsx

--- Atlanta/src/pages/CommonRepairs.tsx
+++ Atlanta/src/pages/CommonRepairs.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header"
 import { DeckRepairProcess } from "@/components/deck-repair-process"
 import { DeckRepairs } from "@/components/deck-repairs"
-import { Deckprocess } from "@/components/deck-process"
+import { DeckProcess } from "@/components/deck-process"
 import { ServicePromise } from "@/components/service-promise"
 import { LocationsSection } from "@/components/locations-section"
 import FAQSection from "@/components/FAQSection"
-import Footer from "@/componenets/Footer"
+import Footer from "@/components/Footer"
 
 const CommonRepairs = () => {
    return (
