fe35c6e02fadaadfb1162abbcdc82c06b0ca5aa5
deckbuilderbuilders-jpg <deckbuilderbuilders@gmail.com>
2025-08-22 07:31:42 -0400

    Update DeckMaintenance.tsx

--- Atlanta/src/components/DeckMaintenance.tsx
+++ Atlanta/src/components/DeckMaintenance.tsx
@@ -24,7 +24,7 @@ const maintenanceServices = [
   },
 ]
 
-export default function DeckMaintenance() {
+export default function deckmaintenance() {
   return (
     <section className="py-16 px-4 bg-white">
       <div className="max-w-7xl mx-auto">
